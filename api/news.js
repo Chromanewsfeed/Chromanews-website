9058a43ac9dfa459b5aa05fc51decbb308167054
Chromanewsfeed <leong.todd@gmail.com>
2026-06-07 10:53:01 -1000

    Update news.js

--- api/news.js
+++ api/news.js
@@ -8,7 +8,7 @@ module.exports = async function handler(req, res) {
   )
   const articles = await response.json()
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate')
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
   res.setHeader('Pragma', 'no-cache')
   res.status(200).json(articles)
 }
